32e571214cc65c40f154e7bafdc5750ae413e7d1
Ar-jay V. Balboa <arjayjay.jc@gmail.com>
2024-04-20 06:57:50 +0000

    Pending changes exported from your codespace

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+function Hello() {
+  return <h1 class='head'>Hello Love G?</h1>;
+}
 
+const container = document.getElementById('mydiv');
+const root1 = ReactDOM.createRoot(container);
+root1.render(<Hello />)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
